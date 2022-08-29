b7e34a3ac5df26bcc870643fc212c47173defeda
KingCobra29A <brunerk@umich.edu>
2022-08-29 06:55:50 -0600

    Add boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Resturant Page',
+            favicon: 'src/noun-food-1750764.svg',
+        }),
+      ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
