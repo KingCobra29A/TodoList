a09e69585e9f0843089ce9190c0dfbc69b8fefe1
KingCobra29A <brunerk@umich.edu>
2022-09-06 09:40:21 -0600

    Add methods to store/load from localStorage

--- src/todo-model/todo-model.js
+++ src/todo-model/todo-model.js
@@ -1,21 +1,56 @@
-const TodoList = (() => {
+import { v4 as uuidv4 } from 'uuid';
+
+export const TodoList = (() => {
     let _todoList = [];
     let _categoryDict = {};
-    let _index = 0;
+    //let _addCallback = todoController.modelCallback;
+    //let _queryCallback = todoController.modelCallback;
 
     const Todo = (title, description, date, id) => {
-        title;
-        description;
-        date;
-        id;
-        category: uncategorized;
-        active: true;
+        return {
+            title,
+            description,
+            date,
+            id,
+            category: "uncategorized",
+            active: true
+        }
+    }
+
+    // TODO: make more robust
+    const _localStorageStoreTodo = (state) => {
+        if(window.localStorage){
+            localStorage.setItem(state.id, JSON.stringify(state));
+        }
+        else{
+            //no local storage
+        }
+    }
+
+
+    // TODO
+    const _localStorageUpdateTodo = () => {
+        //Pretty sure this isnt necessary. _localStorageStoreTodo should do the necessary
+    }
+
+    // Loads the entire contents of localStorage into the _todoList array
+    // TODO: add protection against duplicate entries in the array?
+    const _localStorageLoadTodo = () => {
+        if(window.localStorage){
+            for(let i = 0; i < localStorage.length; i++){
+                _todoList.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+        else{
+            //no local storage
+        }
     }
 
     //Lower order fn used by "add"
     const _addTodo = (title, description, date) => {
-        _todoList.push(Todo(title, description, date, _index));
-        _index += 1; /* HACK? */
+        let newTodo = Todo(title, description, date, uuidv4());
+        _todoList.push(newTodo);
+        _localStorageStoreTodo(newTodo);
     }
 
     //Lower order fn used by "add"
@@ -33,7 +68,7 @@ const TodoList = (() => {
     const add = (method, payload) => {
         if(method == "Todo"){
             //TODO: add validation to these parameters
-            _addTodo(payload.title, payload.description, payload.data);
+            _addTodo(payload.title, payload.description, payload.date);
             //TODO: need to trigger event for view
         }
         else if(method == "Category"){
@@ -42,7 +77,9 @@ const TodoList = (() => {
         }
         else{
             //invalid method
+            return;
         }
+        //_addCallback("add", []); //HACK
     }
 
     //Lower order fn used by "query"
@@ -62,12 +99,10 @@ const TodoList = (() => {
     */
     const query = (method, payload) => {
         if(method == "Category"){
-            _getTodoByCategory(payload.category); /* HACK This is returning a value that is not being used */
-            //TODO: need to trigger event for view
+            //_queryCallback("query", _getTodoByCategory(payload.category)); //Arguements are assumed
         }
         else if(method == "Date"){
-            _getTodoByDate(payload.date); /* HACK This is returning a value that is not being used */
-            //TODO: need to trigger event for view
+            //_queryCallback("query", _getTodoByDate(payload.date)); //Arguements are assumed
         }
         else{
             //invalid method
@@ -100,10 +135,37 @@ const TodoList = (() => {
         //TODO
   }
 
+  /*
+  **
+  **
+  **
+  */
+ const debugPrintState = () => {
+    console.log("Todo State: ")
+    console.log(_todoList);
+ }
+
     return {    add,
                 query,
                 modify,
-                remove
+                remove,
+                debugPrintState,
+                _localStorageLoadTodo,
             }
 
 })();
+
+
+let tempTodos = [
+    {title:"Eat Fudge",description:"2lbs",date:null},
+    {title:"meow",description:"prrrow",date:null},
+    {title:"a",description:"AA",date:null},
+    {title:"b",description:"BB",date:null},
+    {title:"c",description:"CC",date:null},
+    {title:"d",description:"DD",date:null},
+    {title:"e",description:"EE",date:null},
+    {title:"f",description:"FF",date:null},
+    {title:"g",description:"GG",date:null},
+    {title:"h",description:"HH",date:null}
+]
+
