417c9d4007de833103456223c4f84d09c84e2a7a
KingCobra29A <brunerk@umich.edu>
2022-09-14 09:13:59 -0600

    Add styling. Add ability to select, display current category

--- src/todo-view/todo-view.js
+++ src/todo-view/todo-view.js
@@ -5,10 +5,8 @@ import bulletSrc from './assets/noun-circle-5147182.svg';
 
 const TodoView = (() => {
 
-    let bulletPointSrc = "src/todo-view/assets/noun-circle-5147182.svg";
-
-
-
+    let _todoListHeader = document.getElementById("todo-list-header");
+    let _currentCategory = document.querySelector(".menu-active");
     let todoBTN = document.querySelector(".add-todo-btn");
     todoBTN.addEventListener('click', () => {
         
@@ -25,7 +23,7 @@ const TodoView = (() => {
         let innerImg = document.createElement("img");
         wrapperSpan.classList.add("project-icon-placeholder");
         innerImg.classList.add("menu-bullet");
-        innerImg.src = bulletSrc;
+        innerImg.src = imageSrc;
         wrapperSpan.appendChild(innerImg);
         return wrapperSpan;
     }
@@ -40,13 +38,15 @@ const TodoView = (() => {
     }
 
     // lower order fn
-    //used in buildCategoryList and
+    //used in buildCategoryList and ?MAYBESOMETHINGELSE?
     const _buildMenuItem = (label) => {
         let menuItem = document.createElement("li");
         menuItem.classList.add("menu-item");
         menuItem.classList.add("menu-item-sizing");
-        menuItem.appendChild(_buildMenuItemImage(bulletPointSrc));
+        menuItem.appendChild(_buildMenuItemImage(bulletSrc));
         menuItem.appendChild(_buildMenuItemH1(label));
+        menuItem.id = label;
+        menuItem.addEventListener('click', (e) => selectMenu(e.currentTarget));
         return menuItem;
     }
 
@@ -60,9 +60,54 @@ const TodoView = (() => {
         }
     }
 
+
+    //
+    //
+    const _updateTodoListHeader = () => {
+        let textNode = document.createTextNode(_currentCategory.id);
+        let todoListHeaderH1 = _todoListHeader.childNodes[1];
+        todoListHeaderH1.removeChild(todoListHeaderH1.childNodes[0])
+        _todoListHeader.childNodes[1].appendChild(textNode);
+    }
+
+    //
+    //
+    const _removeMenuActiveClass = () => {
+        _currentCategory.classList.remove("menu-active");
+    }
+
+    //
+    //
+    const _setMenuActive = (element) => {
+        element.classList.add("menu-active");
+        _currentCategory = element;
+    }
+
+    //
+    //
+    const selectMenu = (eTarget) => {
+        _removeMenuActiveClass();
+        _setMenuActive(eTarget);
+        _updateTodoListHeader();
+        console.log(eTarget.id)
+    }
+
+    //
+    //
+    const setupMenuEventListeners = () => {
+
+    }
+
     return{
         buildCategoryList,
     }
 })();
 
 TodoView.buildCategoryList();
+
+/*
+Event listener
+remove all other active statuses
+add new active status
+modify todo-list
+*/
